d1c2a7f03b9276ec4bbc616522649a74d3f3bee0
yeldosyaya <yeldos.t@yaya.kz>
2024-12-16 20:00:35 +0500

    refactoring useFetching

---
+++ src/hooks/types.ts
@@ -0,0 +1,8 @@
+import { AxiosError, AxiosResponse } from "axios";
+
+export type FetchingParams<T, D, E> = {
+  query: (arg?: any) => Promise<AxiosResponse<T, D>>;
+  onComplete?: (response: AxiosResponse<T, D>) => void;
+  onError?: (error: AxiosError<E>) => void;
+  initialLoading?: boolean;
+};
--- src/hooks/useFetching.ts
+++ src/hooks/useFetching.ts
@@ -1,34 +1,28 @@
-import { AxiosError, AxiosResponse } from "axios";
 import { useState } from "react";
-
-type useFetchingTypes<T, D, E> = {
-  query: (arg: any) => Promise<AxiosResponse<T, D>>;
-  onComplete?: (response: AxiosResponse<T, D>) => undefined;
-  onError?: (error: AxiosError<E>) => undefined;
-  initialLoading?: boolean;
-};
+import { AxiosError, AxiosResponse } from "axios";
+import { FetchingParams } from "./types";
 
 export const useFetching = <T = any, D = any, E = any>({
   query,
   onComplete,
   onError,
   initialLoading = false,
-}: useFetchingTypes<T, D, E>) => {
+}: FetchingParams<T, D, E>) => {
   const [data, setData] = useState<AxiosResponse<T, D> | null>(null);
   const [error, setError] = useState<AxiosError<E> | null>(null);
   const [loading, setLoading] = useState(initialLoading);
 
-  const fetch = async (props: any = null) => {
+  const fetch = async (props?: any) => {
+    setLoading(true);
+    setError(null);
     try {
-      setLoading(true);
-      if (!query) return;
       const response = await query(props);
       setData(response);
-      onComplete && onComplete(response);
+      onComplete?.(response);
     } catch (error) {
-      const axiosError = error as AxiosError<any>;
+      const axiosError = error as AxiosError<E>;
       setError(axiosError);
-      onError && onError(axiosError);
+      onError?.(axiosError);
     } finally {
       setLoading(false);
     }
